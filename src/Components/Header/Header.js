73c9f7de7dfbc99123806078a01e4289607c1bfb
gvns0x <gvns0x@gmail.com>
2024-09-16 22:59:50 +0100

    fix colors

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,15 @@ import React from 'react';
 import '../Header/Header.css'
 import Logo from '../../Images/logo.png'
 import CTA from '../../Components/CTA/CTA.js'
+import LogoText from '../../Images/logotext.png'
 
 const Header = () => {
     return (
         <div className='Header'>
+            <div className='Header-content'>
             <img className="Logo" src={Logo}/>
+            <img className="LogoText" src={LogoText}/>
+            </div>
             <CTA/>
             
         </div>
