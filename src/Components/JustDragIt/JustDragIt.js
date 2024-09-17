e7f9b84306d330af3738cd77a6ab4b565b1b3263
gvns0x <gvns0x@gmail.com>
2024-09-17 13:17:44 +0100

    new copy for rahul

--- src/Components/JustDragIt/JustDragIt.js
+++ src/Components/JustDragIt/JustDragIt.js
@@ -8,7 +8,7 @@ const JustDragIt = () => {
             <h2 className='JustDragIt-title' id='JustDragIt-1'>Is mom sleeping now?</h2>
             <h2 className='JustDragIt-title' id='JustDragIt-2'>If it’s 3pm here, what time is it for Mero?</h2>
             <h2 className='JustDragIt-title' id='JustDragIt-3'>When is it a good time to call Fran?</h2>
-            <h2 className='JustDragIt-title' id='JustDragIt-4'>Rahul is in the US now, what time is it there?</h2>
+            <h2 className='JustDragIt-title' id='JustDragIt-4'>Rahul landed in the US, what time is it there?</h2>
             <div className='JustDragIt-content'>
                 <h2 className='JustDragItFinal'>Just drag</h2>
                 <img className="DragSymbol" src={DragSymbol}/>
