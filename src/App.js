5a1bb562b2d66e3cce42698f7d55a942c364b885
gvns0x <gvns0x@gmail.com>
2024-09-20 14:42:00 +0100

    fix automatically copy

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <JustDragIt/>
         <div className='Feature-container'>
           <Feature title="Add people in any timezone" description="All you need is someone’s name and their location. " image={Timezones} />
-          <Feature title="Group people who are in the same time" description="To avoid display duplicate times, people in the same time are automatically grouped." image={Grouping} />
+          <Feature title="Automatically group people in the same time" description="To avoid display duplicate times, people in the same time are automatically grouped." image={Grouping} />
           <Feature title="Easily convert timezones by dragging" description="With one finger, drag anyone’s time and see the impact on others’ time in their timezone." image={Drag} />
           <Feature title="Reset to current time by double-tapping anywhere" description="Always go back to the current time by double-tapping anywhere." image={Reset} />
           <Feature title="Define availability windows" description="You can define when people are available. Some people use it for worktimes (8am - 6pm), some use it for when their friends are sleeping (10pm - 8am)." image={Availability} />
