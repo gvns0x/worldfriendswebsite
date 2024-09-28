a46b9de5b99ba791bf8d932e10abe0ad0dadaa00
gvns0x <gvns0x@gmail.com>
2024-09-28 14:59:22 +0100

    fix lint

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React/*, { useState, useEffect }*/ from 'react';
 import '../Header/Header.css'
 import Logo from '../../Images/logo.png'
 // import CTA from '../../Components/CTA/CTA.js'
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React/*, { useState, useEffect } */ from 'react';
 import './Hero.css';
 // import QRCode from '../QRCode/QRCode';
 import HeroPhone from '../../Images/herophone.png';
