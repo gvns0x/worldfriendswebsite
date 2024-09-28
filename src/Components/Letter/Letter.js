f5a4966f951df128137b439d8eaa020e0bc37981
gvns0x <gvns0x@gmail.com>
2024-09-28 15:52:18 +0100

    fix error

--- src/Components/Letter/Letter.js
+++ src/Components/Letter/Letter.js
@@ -11,7 +11,7 @@ As an immigrant with friends, family, and colleagues all over the globe 🌍, it
 <br/><br/>
 Globezones <img className="GlobezonesSentenceLogo" alt="" src={Logo}/> helps me see when my friends are available and make plans by easily dragging their time into the future.
 <br/><br/>
-I use it every day. Hopefully it can be useful for you to.
+I use it every day. Hopefully it can be useful for you too.
 <br/><br/>
 If you are using globezones, I’d love to know about it. Hopefully you have feedback that can help improve it. <a className="LetterLink" href="https://x.com/gvns0x" rel="noopener noreferrer" target='_blank'>DM me on X.</a>
 <br/><br/>
