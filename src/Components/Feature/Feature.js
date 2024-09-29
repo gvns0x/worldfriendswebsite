125bb1577ed649bf83702da22ea349b613419c1a
gvns0x <gvns0x@gmail.com>
2024-09-29 17:49:31 +0100

    add video

--- src/Components/Feature/Feature.js
+++ src/Components/Feature/Feature.js
@@ -1,13 +1,20 @@
 import './Feature.css';
 
-function Feature({ title, description, image, titleColor, descriptionColor, bgColor }) {
+function Feature({ title, description, image, titleColor, descriptionColor, bgColor, videostyle }) {
     return (
         <div style={{ backgroundColor: bgColor }} className="Feature">
             <div className='Feature-content'>
                 <h3 style={{ color: titleColor }}>{title}</h3>
                 <p style={{ color: descriptionColor }}>{description}</p>
             </div>
-            <img src={image} alt={title} />
+            {image.endsWith('.mp4') ? (
+                <video className="FeatureVideo" autoPlay loop muted playsInline controls={false}>
+                    <source src={image} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            ) : (
+                <img src={image} alt={title} />
+            )}
         </div>
     );
 }
