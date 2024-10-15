661e8491753109cc9eca294f4d841958e0645042
gvns0x <gvns0x@gmail.com>
2024-10-15 20:35:20 +0530

    change copy

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App() {
         <Hero/>
         <JustDragIt/>
         <div className='Feature-container'>
-          <Feature title="Add1 people in any timezone" description="All you need is someone’s name and their location. " image={Timezones} />
-          <Feature title="Automatically group people in the same time" description="To avoid display duplicate times, people in the same time are automatically grouped." image={Grouping} />
+          <Feature title="Add people in any timezone" description="All you need is someone’s name and their location. " image={Timezones} />
+          <Feature title="Automatically group with the same time and availability" description="To avoid display duplicate times, people in the same time are automatically grouped." image={Grouping} />
           <Feature title="Easily convert timezones by dragging" description="With one finger, drag anyone’s time and see the impact on others’ time in their timezone." image={DragVideo} videostyle="500px"/>
           <Feature title="Reset to current time by double-tapping anywhere" description="Always go back to the current time by double-tapping anywhere." image={Reset} />
           <Feature title="Define availability windows" description="You can define when people are available. Some people use it for worktimes (8am - 6pm), some use it for when their friends are sleeping (10pm - 8am)." image={Availability} />
