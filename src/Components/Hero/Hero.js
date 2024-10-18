ede0b4dda2837dafa2e06e41e9e814b85bc29456
gvns0x <gvns0x@gmail.com>
2024-10-18 09:43:56 +0530

    add qr code to website

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,16 +1,16 @@
-import React/*, { useState, useEffect } */ from 'react';
+import React, { useState, useEffect }  from 'react';
 import './Hero.css';
-// import QRCode from '../QRCode/QRCode';
+import QRCode from '../QRCode/QRCode';
 import HeroPhone from '../../Images/herophone.png';
 
 const Hero = () => {
-    /*const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         const handleResize = () => setWindowWidth(window.innerWidth);
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
-    }, []);*/
+    }, []);
 
     return (
         <div className='Hero'>
@@ -18,8 +18,8 @@ const Hero = () => {
                 <div className="Hero-text-container">
                     <h1>Visualize and convert timezones for friends, family, and colleagues.</h1>
                     <div className="Hero-conditional-container">
-                        <p className='TestFlight-content'>Globezones is available on TestFlight. Do you want to try it? <a href="https://x.com/gvns0x" rel="noopener noreferrer" target='_blank'>DM me on X</a> and I'll give you access :)</p>
-                        {/* <div className="Hero-conditional">
+                        {/*<p className='TestFlight-content'>Globezones is available on TestFlight. Do you want to try it? <a href="https://x.com/gvns0x" rel="noopener noreferrer" target='_blank'>DM me on X</a> and I'll give you access :)</p>*/}
+                        <div className="Hero-conditional">
                             
                             {windowWidth > 500 ? (
                             <QRCode/>
@@ -30,11 +30,11 @@ const Hero = () => {
                     {windowWidth > 500 ? (
                     <div className="Hero-subtext-container">
                     <p className="Hero-subtext">Scan the QR to download for iPhone</p>
-                    <a rel="noreferrer" href="http://globezones.com/" className="Hero-subtext">or visit the AppStore</a>
+                    <a rel="noreferrer" href="https://apps.apple.com/gb/app/globezones/id6714448163" className="Hero-subtext">or visit the AppStore</a>
                     </div>
                     ) : (
                         " "
-                    )}*/}
+                    )}
                 </div>
                 </div>
                     <img src={HeroPhone} alt="iPhone with the Globezones app" className="Hero-image" />
